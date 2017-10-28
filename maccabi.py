c762d9147eb4d0ba947f082704b6aa2a5981c3d2
Ori Kalinski <ori@mintigo.com>
2017-10-28 20:21:12 +0300

    need to be fixed (hmm hmm yana gofman)

--- maccabi.py
+++ maccabi.py
@@ -1,42 +1,36 @@
 import strings
 import dryscrape
 
-class GetDoctorsList(object):
 
-    search_by_to_intervals  = {strings.AREA_OF_TREATMENT:(1, 1)}
+class ParamMapper(object):
+    search_by_to_intervals = {strings.AREA_OF_TREATMENT: (1, 1)}
+
     def __init__(self, search_by):
         self.base_url = "http://serguide.maccabi4u.co.il/heb/Doctors/DoctorsSearchResults/"
         self.search_by = search_by
-        self.session = dryscrape.session()
-        self.search_field_information = {strings.AREA_OF_TREATMENT: (1,1)}
+        self.session = dryscrape.Session()
+        self.search_field_information = {strings.AREA_OF_TREATMENT: (1, 1)}
 
-    def search_by_param(self):
+    def map_param(self):
         mapping = dict()
         curr_field_xpath = "//section[@id='SearchParams']/span[@class='ng-binding ng-scope']"
-        start_point = self.search_field_information[self.search_by][0]
-        iterate_by = self.search_field_information[self.search_by][1]
-        self.session.visit(self.base_url)
+        start_point, iterate_by = self.search_field_information[self.search_by]
         value_name = self.session.at_xpath("//section[@class=error_section ng-binding ng-scope")
 
-        while value_name == None:
-            if(len(self.search_field_information[self.search_by[0]]) == 1):
-                start_point = "00" + str(start_point)
-            elif (len(self.search_field_information[self.search_by[0]]) == 2):
-                start_point = "0" + str(start_point)
-            self.session.visit(self.base_url + "?" + self.search_by + start_point)
+        while value_name is None:
+            self.session.visit("%s?%s%s" % (self.base_url, self.search_by, str(start_point).zfill(3)))
             value_name = self.session.at_xpath(curr_field_xpath)
-            mapping[value_name] = int(start_point)
+            mapping[value_name] = start_point
             start_point += iterate_by
 
         return mapping
 
 
+def main():
+    mapper = ParamMapper(strings.AREA_OF_TREATMENT)
+    mapping = mapper.map_param()
+    print(mapping)
 
 
-
-        
-
-
-
-
-
+if __name__ == '__main__':
+    main()
